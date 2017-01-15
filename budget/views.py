5af540c628b5e57c0b9610eebacd9f5e5d1e3eb8
andrew.willey <andrew@apt9online.com>
2017-01-15 17:40:43 -0600

    basic templates and auth

--- budget/views.py
+++ budget/views.py
@@ -1,3 +1,21 @@
+
+from django.http import Http404
 from django.shortcuts import render
 
-# Create your views here.
+from .models import *
+
+def ofx_loader(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            #handle the File
+            request.FILES['file']
+            return HttpResponseRedirect('')
+    
+    else:
+        form = UploadFileForm()
+    
+    return render(request, 'upload.html')
+    context = {}
+
+    return render(request, 'polls/detail.html', context)
