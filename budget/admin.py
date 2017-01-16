1d425e19d2af8f412db9774cfcef14c8b06c76ca
andrew.willey <andrew@apt9online.com>
2017-01-16 08:20:04 -0600

    going mobile

--- budget/admin.py
+++ budget/admin.py
@@ -1,7 +1,31 @@
 from django.contrib import admin
 from django.apps import apps
 
-app = apps.get_app_config('budget')
+from budget.models import *
 
+class CategoryInline(admin.TabularInline):
+    model = Category
+    extra = 0
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [CategoryInline,
+    ]
+
+    def get_queryset(self, request):
+        qs = super(CategoryAdmin, self).get_queryset(request)
+        return qs.filter(parent__isnull=True)
+
+admin.site.register(Category, CategoryAdmin)
+
+class MerchantCategoryCodeAdmin(admin.ModelAdmin):
+    search_fields = ['mcc_combined_description']
+    ordering = ('mcc_id',)
+
+admin.site.register(MerchantCategoryCode, MerchantCategoryCodeAdmin)
+
+
+app = apps.get_app_config('budget')
 for model_name, model in app.models.items():
-    admin.site.register(model)
+    if not admin.site.is_registered(model):
+        admin.site.register(model)
+
--- budget/models.py
+++ budget/models.py
@@ -1,16 +1,39 @@
 from django.db import models
 from django.conf import settings
 
+from dateutil.relativedelta import relativedelta
+
 from simple_history.models import HistoricalRecords
 from django_extensions.db.models import (TitleSlugDescriptionModel, TimeStampedModel)
 
+
+
 #will need to initialize codes with from ofxparse.mcc import codes
 class MerchantCategoryCode(models.Model):
-    mcc_id = models.IntegerField
+    mcc_id = models.IntegerField()
     mcc_combined_description = models.TextField(max_length=255, blank=True)
     mcc_usda_description = models.TextField(max_length=255, blank=True)
     mcc_irs_description = models.TextField(max_length=255, blank=True)
-    mcc_reportable = models.TextField(max_length=255, blank=True)
+    mcc_reportable = models.TextField(max_length=25, blank=True)
+
+    class Meta:
+        ordering = ['mcc_id']
+
+    def __str__(self):
+        return '%s %s' % (self.mcc_id, self.mcc_combined_description)
+
+
+def mcc_init():
+    from ofxparse.mcc import codes
+    for code, value in codes.items():
+        m = MerchantCategoryCode.objects.get_or_create(
+            mcc_id = code,
+            mcc_combined_description = codes[code].get('combined description'),
+            mcc_usda_description = codes[code].get('USDA description'),
+            mcc_irs_description = codes[code].get('IRS Description'),
+            mcc_reportable = codes[code].get('Reportable under 6041/6041A and Authority for Exception')
+        )
+
 
 class Account(TimeStampedModel):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL)
@@ -53,6 +76,13 @@ class Category(TimeStampedModel):
     
     class Meta:
         verbose_name_plural = "categories"
+        #ordering = ['name']
+
+    def __str__(self):
+        n = ''
+        if self.parent:
+            n = self.parent.name + ' - '
+        return n + self.name
 
 class Budget(TimeStampedModel):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL)
@@ -66,27 +96,46 @@ class BudgetItemStatus(models.Model):
     class Meta:
         verbose_name_plural = 'budget item statuses'
         
-class BudgetReserve(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
-    title = models.CharField
-
 class PayeeXref(models.Model):
     name = models.CharField(max_length=255)
     transaction = models.ManyToManyField(Transaction)
 
+class BudgetReserve(models.Model):
+    budget = models.ForeignKey(Budget)
+    
+    title = models.CharField(max_length=255)
+    target_amount = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
+
+
 class BudgetItem(TimeStampedModel):
     budget = models.ForeignKey(Budget)
     payee = models.ForeignKey(PayeeXref, blank=True, null=True)
-    budget_item_date = models.DateField(help_text='Projected date for allocation', blank=True)
+    
+    budget_item_date = models.DateTimeField(help_text='Projected date for allocation', blank=True)
+    budget_item_enddate = models.DateTimeField(help_text='If a ranged budget item, provide an end date', blank=True)
+    
     reserve = models.ForeignKey(BudgetReserve, blank=True)
     category = models.ForeignKey(Category)
 
-    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    target_amount = models.DecimalField(max_digits=8, decimal_places=2)
 
     notes = models.CharField(max_length=255)
 
     status = models.ForeignKey(BudgetItemStatus)
 
+    def enddate_offset(self, *args, **kwargs):
+        '''Pass offset_days, offset_months, and offset_years as arguments to set the range end'''
+        r = self.budget_item_date
+        r = ( r  
+            + relativedelta(days=kwargs.get('offset_days') or 0) 
+            + relativedelta(months=kwargs.get('offset_months') or 0)
+            + relativedelta(years=kwargs.get('offset_years') or 0) 
+        )
+        r.second = 0
+        r.microsecond = 0
+        self.budget_item_enddate = r
+        self.save()
+        
 
 class TransactionAllocation(models.Model):
     transaction = models.ForeignKey(Transaction)
